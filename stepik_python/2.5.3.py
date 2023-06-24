962014d2002fa288851fd2c3e860586d06b9bfa4
Yakov Mishin Windows <qwqw@ewew.com>
2023-06-24 13:16:02 +0300

    solved task 2.5.3

--- stepik_python/2.5.3.py
+++ stepik_python/2.5.3.py
@@ -2,12 +2,6 @@ a = [int(i) for i in input().split()]
 a.sort()
 s = []
 for i in a:
-    print('a_do=', a, ' i=', i, end=' ')
-    if a.count(i) > 1:
+    if (i not in s) and (a.count(i)>1):
         s += [i]
-        while i in a:
-            a.remove(i)
-            #print('a', a, end='')
-    print('s=', s)
-    print('a_posle=', a)
 print(*s)
