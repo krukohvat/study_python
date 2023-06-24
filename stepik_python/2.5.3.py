7ba05cc70c77eebb78c33a8d37b0054c0384ef8d
Yakov Mishin Windows <qwqw@ewew.com>
2023-06-24 12:22:24 +0300

    add strings tasks, before task2.5.3 branch

--- stepik_python/2.5.3.py
+++ stepik_python/2.5.3.py
@@ -1,1 +1,13 @@
 a = [int(i) for i in input().split()]
+a.sort()
+s = []
+for i in a:
+    print('a_do=', a, ' i=', i, end=' ')
+    if a.count(i) > 1:
+        s += [i]
+        while i in a:
+            a.remove(i)
+            #print('a', a, end='')
+    print('s=', s)
+    print('a_posle=', a)
+print(*s)
