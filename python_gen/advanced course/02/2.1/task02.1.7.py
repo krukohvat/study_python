a980b2016d3f7cb8f85558b3ee6ca2b6260e8c51
Yakov Mishin Windows <qwqw@ewew.com>
2023-06-28 12:24:05 +0300

    solve task02.1.7 py_gen_adv_course

--- python_gen/advanced course/02/2.1/task02.1.7.py
+++ python_gen/advanced course/02/2.1/task02.1.7.py
@@ -1,4 +1,22 @@
-chislo = [i for i in input()]
-#chislo.extend(input())
-
-print(chislo)
+"""
+На вход программе подаётся натуральное число. Напишите программу, которая вставляет в заданное число запятые в соответствии со стандартным американским соглашением о запятых в больших числах.
+"""
+ch = int(input())
+sp = []
+if ch >= 1000:
+    while ch//1000 > 0:
+        if ch%1000 > 100:
+            sp.append(ch%1000)
+        else:
+            if ch%1000 >= 10:
+                sp.append(ch%1000)
+                sp[len(sp)-1] = '0'+str(sp[len(sp)-1])
+            else:
+                sp.append(ch%1000)
+                sp[len(sp)-1] = '00'+str(sp[len(sp)-1])
+        ch = ch//1000
+    sp.append(ch)
+    sp = sp[::-1]
+    print(*sp, sep=',')
+else:
+    print(ch)
